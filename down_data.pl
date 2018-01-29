0a1514bab92908c41b2e17eb5d764487dfab98eb
bioinfoplant <mattia.b1@gmail.com>
2018-01-29 16:16:15 +0100

    Deal with not unknown entries or characters

--- down_data.pl
+++ down_data.pl
@@ -12,6 +12,7 @@ if 0;
  
 use strict;
 use LWP;
+use Encode;
 
 my $client = LWP::UserAgent->new('agent' => 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:14.0) Gecko/20100101 Firefox/14.0.1', keep_alive => 1, timeout => 30);
 my $base = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/';
@@ -20,20 +21,26 @@ my $query;
 my @acc;
 my $data;
 my $api_option = ''; # Insert here the API key Registered through NCBI account
+my $chunk_size = 50;
 
-open (INPUT_FILE, "<", 'accnumber.txt') || die "Can't open file!\n";
-open (OUTPUT_FILE, ">", 'data.txt') || die "Can't write file!\n";;
+open (INPUT_FILE, "<", 'accession_numbers.txt') || die "Can't open file!\n";
+open (OUTPUT_FILE, ">", 'ncbi_data.txt') || die "Can't write file!\n";;
 
 print "Loading data..\n";
-foreach (<INPUT_FILE>){
-	$_ =~ s/[^\w\.]//g;
-	push (@acc, $_);
+while (<INPUT_FILE>){
+	my $ustring = decode( 'UTF-8', $_ ); #Avoid wide characters in the accession list
+	$ustring =~ s/\W+//;
+	$ustring =~ s/.+\://;
+	push (@acc, $ustring);
 }
 
 close INPUT_FILE;
 
+print @acc;
+
+
 my $n;
-while ( my @chunk = splice (@acc, 0, 50) ) {
+while ( my @chunk = splice (@acc, 0, $chunk_size) ) {
 	++$n;
 	print "Processing Chunk $n\n";
 	$query = join (",", @chunk);
@@ -47,24 +54,35 @@ exit;
 
 sub download {
 	#assemble the esearch URL
-	my $esearch_url = $base . "esearch.fcgi?db=nuccore&term=$query&usehistory=y$api_option";
-	print "$esearch_url\n";
+	my $esearch_url = $base . "esearch.fcgi?db=nuccore&term=$query&usehistory=y$api_option&retmax=$chunk_size";
+	print "	$esearch_url\n";
 	
 	print "	Searching..\n";
 	my $esearch = $client->get($esearch_url);
 	print $esearch->status_line unless $esearch->is_success;
 	my $esearch_response = $esearch->decoded_content;
+	# print "\n*****\n$esearch_response\n*****\n";
+	
+	my @errors = ();
+	while ($esearch_response =~ m/<PhraseNotFound>(\S+)<\/PhraseNotFound>/g){
+		push (@errors, $1);
+	}
+	
+	if (@errors){
+		die "\nWARNING: The following term(s) gave errors: @errors\n\nPlease correct the list and perform the download again.\n";
+	}
+	
 	sleep(0.5);
 	
 	#parse WebEnv, QueryKey and Count (# records retrieved)
-	my $web = $1 if ($esearch_response =~ /<WebEnv>(\S+)<\/WebEnv>/);
-	my $key = $1 if ($esearch_response =~ /<QueryKey>(\d+)<\/QueryKey>/);
-	my $count = $1 if ($esearch_response =~ /<Count>(\d+)<\/Count>/);
+	my $web = $1 if ($esearch_response =~ m/<WebEnv>(\S+)<\/WebEnv>/);
+	my $key = $1 if ($esearch_response =~ m/<QueryKey>(\d+)<\/QueryKey>/);
+	my $count = $1 if ($esearch_response =~ m/<Count>(\d+)<\/Count>/);
 	
 	print "	Downloading..\n";
 	my $efetch_url = $base ."efetch.fcgi?db=nuccore&WebEnv=$web&query_key=$key&rettype=gb&retmode=text$api_option";
 	my $efetch = $client->get($efetch_url);
-	print $efetch->status_line unless $efetch->is_success;
+	# print $efetch->status_line unless $efetch->is_success;
 	sleep(0.5);
 	
 	print "	Saving..\n";
